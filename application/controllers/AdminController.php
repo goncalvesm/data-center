4b2e48db653b3e0d2723eeb3281c3ec59c2411bd
mickael goncalves <mickael.goncalves@webtales.fr>
2013-01-10 15:23:18 +0100

    Implementation du changement de formule, ajout de composer et envois du mail lors du changement de formule

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -1,6 +1,23 @@
 <?php
 
 class AdminController extends Zend_Controller_Action {
+		
+	protected function _rrmdir($dir) {
+		if (is_dir($dir)) {
+			$objects = scandir($dir);
+			foreach ($objects as $object) {
+				if ($object != "." && $object != "..") {
+					if (filetype($dir."/".$object) == "dir"){
+						$this->_rrmdir($dir."/".$object); 
+					} else {
+						unlink($dir."/".$object);
+					}
+				}
+			}
+			reset($objects);
+			return rmdir($dir);
+		} 
+	}	
 	
 	public function init() {
 		$this->_auth = new Zend_Perso_Authentification;
@@ -24,6 +41,14 @@ class AdminController extends Zend_Controller_Action {
 			$this->_session->set('erreur', 'Vous devez vous connecter pour utiliser les fonctionnalités');
 			$this->_helper->redirector('index', 'index');
 		}
+		
+		//Envois du message stocké en session à la vue
+		$this->view->message = $this->_session->get("message");
+		$this->_session->set('message', '');
+		
+		//Envois de l'erreur stockée en session à la vue
+		$this->view->erreur = $this->_session->get("erreur");
+		$this->_session->set('erreur', '');
 	}
 
 	public function indexAction() {
@@ -33,12 +58,11 @@ class AdminController extends Zend_Controller_Action {
 		$contenu .= "	<table class='table table-striped table-hover'>
 							<thead>
 								<tr>
-									<th class='nom'>Nom</th>
-									<th class='prenom'>Prenom</th>
-									<th class='pseudo'>Pseudo</th>
-									<th class='droit'>Droit</th>
-									<th class='espaceDispo'>Espace disponnible</th>
-									<th class='actions'>Actions</th>
+									<th>Pseudo</th>
+									<th>Droit</th>
+									<th>Formule</th>
+									<th>Espace disponnible</th>
+									<th>Actions</th>
 								</tr>
 							</thead>
 							<tbody>";
@@ -53,10 +77,9 @@ class AdminController extends Zend_Controller_Action {
 			$pourcentage = (string)$poucentage."%";
 			
 			$contenu .= "	<tr>
-								<td>".$value['nom']."</td>
-								<td>".$value['prenom']."</td>
 								<td>".$value['pseudo']."</td>
 								<td>".$value['droit']."</td>
+								<td>".$value['formule']." Mo</td>
 								<td class='centrer'>";
 								if($value['droit'] == "utilisateur"){
 									$contenu .= "<div class=\"progress progress-striped active\">
@@ -69,7 +92,7 @@ class AdminController extends Zend_Controller_Action {
 			$contenu .=	"		</td>
 								<td>
 									<a href=\"/admin/supprimer-utilisateur?id=".$value['pseudo']."\"><img src=\"/images/supp_user.png\" title=\"Supprimer l'utilisateur\" /></a>
-									<a href=\"/admin/editer-utilisateur?id=".$value['pseudo']."\"><img src=\"/images/edit_user.png\" title=\"Editer l'utilisateur/Modifier formule\" /></a>
+									<a href=\"/admin/editer-utilisateur?id=".$value['pseudo']."\"><img src=\"/images/edit_user.png\" title=\"Modifier la formule\" /></a>
 								</td>
 							</tr>";	
 		}
@@ -87,10 +110,106 @@ class AdminController extends Zend_Controller_Action {
 	}
 	
 	public function editerUtilisateurAction() {
+		$params = $this->getAllParams();
 		
+		if(isset($params['id'])){
+			//Envois du formulaire de changemeent d'option à la vue
+			$changementOption = new Application_Form_ChangementOption();
+			$this->view->changementOption = $changementOption;
+			
+			$pseudo = $params['id'];
+			
+			$user = $this->_sqlite->execute("SELECT * FROM utilisateurs WHERE pseudo='".$pseudo."'");
+			$user = $user[0];
+			
+			$this->view->formule = $user['formule'];
+			$this->view->changementOption = $changementOption;
+			$this->view->user = $pseudo;
+		} else {
+			$this->_session->set('erreur', 'Vous ne pouvez pas editer un utilisateur sans fournir son pseudo');
+			$this->_helper->redirector('index', 'admin');
+		}
 	}
 	
 	public function supprimerUtilisateurAction() {
+		$params = $this->getRequest()->getParams();
 		
+		if(isset($params['id'])){
+			$pseudo = $params['id'];
+		} else {
+			$this->_session->set('erreur', 'Vous ne pouvez pas supprimer un utilisateur sans fournir son pseudo');
+			$this->_helper->redirector('index', 'admin');
+		}
+		
+		$root = $this->_sqlite->execute("SELECT * FROM dossiers WHERE utilisateur='".$pseudo."' AND root='1'");
+		
+		if(is_array($root)){
+			if(is_array($this->_sqlite->execute("DELETE FROM utilisateurs WHERE pseudo='".$pseudo."'"))){
+				if(is_array($this->_sqlite->execute("DELETE FROM dossiers WHERE utilisateur='".$pseudo."'"))){
+					if(is_array($this->_sqlite->execute("DELETE FROM fichiers WHERE utilisateur='".$pseudo."'"))){
+						if($this->_rrmdir(APPLICATION_PATH."/../data/".$root[0]['chemin'])) {
+							$this->_session->set('message', 'utilisateur supprimé avec succés');
+							$this->_helper->redirector('index', 'admin');
+						} else {
+							$this->_session->set('erreur', 'Erreur lors de la suppression des fichiers de l\'utilisateur sur le serveur');
+							$this->_helper->redirector('index', 'admin');
+						}
+					} else {
+						$this->_session->set('erreur', 'Erreur lors de la suppression des fichiers de l\'utilisateur en base de données');
+						$this->_helper->redirector('index', 'admin');
+					}
+				} else {
+					$this->_session->set('erreur', 'Erreur lors de la suppression des dossiers de l\'utilisateur en base de données');
+					$this->_helper->redirector('index', 'admin');
+				}
+			} else {
+				$this->_session->set('erreur', 'Erreur lors de la suppression de l\'utilisateur en base de données');
+				$this->_helper->redirector('index', 'admin');
+			}
+		} else {
+			$this->_session->set('erreur', 'Erreur lors de la récupération du fichier racine de l\'utilisateur');
+			$this->_helper->redirector('index', 'admin');
+		}
+	}
+
+	public function changerFormuleAction() {
+		$params = $this->getRequest()->getParams();
+		
+		if(isset($params['pseudo'])){
+			$pseudo = $params['pseudo'];
+		} else {
+			$this->_session->set("erreur", "Vous ne pouvez pas changer la formule d'un utilisateur sans fournir son pseudo");
+			$this->_helper->redirector->gotoUrl("/admin/index");
+		}
+		
+		if($params['bootstrap']['dixMo'] == 1) {
+			if(is_array($this->_sqlite->execute("UPDATE utilisateurs SET 'formule'='10' WHERE pseudo='".$user['pseudo']."'"))){
+				$mail = new Zend_Mail();
+				$mail->setBodyText('Vôtre compte possède desormais 10 Mo d\'espace de stockage');
+				$mail->setFrom('contact@datacenter.fr', 'Administrateur datacenter');
+				$mail->addTo('mickael.goncalves@webtales.fr', 'utilisateur');
+				$mail->setSubject('Changement de formule');
+				$mail->send();
+				
+				$this->_session->set("message", "Ce compte possède désormais 10 Mo d'espace de stockage !");
+			} else {
+				$this->_session->set("erreur", "Erreur lors du changement de la formule en base de données");
+			}
+		} elseif($params['bootstrap']['centMo'] == 1) {
+			if(is_array($this->_sqlite->execute("UPDATE utilisateurs SET 'formule'='100' WHERE pseudo='".$user['pseudo']."'"))){
+				$mail = new Zend_Mail();
+				$mail->setBodyText('Vôtre compte possède desormais 100 Mo d\'espace de stockage');
+				$mail->setFrom('contact@datacenter.fr', 'Administrateur datacenter');
+				$mail->addTo('mickael.goncalves@webtales.fr', 'utilisateur');
+				$mail->setSubject('Changement de formule');
+				$mail->send();
+				
+				$this->_session->set("message", "Ce compte possède désormais 100 Mo d'espace de stockage !");
+			} else {
+				$this->_session->set("erreur", "Erreur lors du changement de la formule en base de données");
+			}
+		}
+		
+		$this->_helper->redirector->gotoUrl("/admin/index");
 	}
 }
